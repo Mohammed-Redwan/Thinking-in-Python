d6ff4bdf6a82910912fbf1c1f5622c662ab0d2ea
Mohammed Redwan <mohammed.redwan.pro@gmail.com>
2025-09-18 10:11:21 +0200

    Assignment 1

---
+++ Assignment1.py
@@ -0,0 +1,6 @@
+
+# Input and Output Methods
+name = input("Enter your name : \n")
+age = input("Enter you age : \n")
+age = int(age) - 5
+print("Hello beautiful " + name + " , your age is " + str(age) + "!")
